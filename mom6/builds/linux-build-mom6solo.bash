6c2e31e6712ad8180ad1ca4b40e2eac3b89f9b8b
Niki Zadeh <Niki.Zadeh@noaa.gov>
2019-02-25 16:25:06 -0500

    Enahncing the build script

--- mom6/builds/linux-build-mom6solo.bash
+++ mom6/builds/linux-build-mom6solo.bash
@@ -3,8 +3,8 @@
 #platform="intel18"
 #machine_name="tiger" 
 #platform="intel18"
-#machine_name="googcp" 
-#platform="intel19"
+machine_name="googcp" 
+platform="intel19"
 #machine_name = "ubuntu"
 #platform     = "pgi18"                                             
 #machine_name="ubuntu" 
@@ -15,16 +15,32 @@
 #platform     = "gnu6" 
 #machine_name = "theta"   
 #platform     = "intel16"
-machine_name="lscsky50"
-platform="intel19up2_avx1" #"intel18_avx1" # "intel18up2_avx1" 
-
+#machine_name="lscsky50"
+#platform="intel19up2_avx1" #"intel18_avx1" # "intel18up2_avx1" 
 target="prod" #"debug-openmp"       
+
+usage()
+{
+    echo "usage: linux-build-mom6solo.bash -m googcp -p intel19 -t prod"
+}
+
+# parse command-line arguments
+while getopts "m:p:t:h" Option
+do
+   case "$Option" in
+      m) machine_name=${OPTARG};;
+      p) platform=${OPTARG} ;;
+      t) target=${OPTARG} ;;
+      h) usage ; exit ;;
+   esac
+done
 
 rootdir=`dirname $0`
 abs_rootdir=`cd $rootdir && pwd`
 
 
 #load modules              
+source $MODULESHOME/init/bash
 source $rootdir/$machine_name/$platform.env
 . $rootdir/$machine_name/$platform.env
 
@@ -55,6 +71,11 @@ $srcdir/mkmf/bin/list_paths $srcdir/FMS
 $srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names
 
 make $makeflags libfms.a         
+
+if [ $? -ne 0 ]; then
+   echo "Could not build the FMS library!"
+   exit 1
+fi
 
 popd
 
